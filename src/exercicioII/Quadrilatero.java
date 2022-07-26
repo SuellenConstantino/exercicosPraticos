b75b4a0bac659d38c3ff0755d89a9b1aa6d858dc
Su Constantino <suellen.ds.constantino@gmail.com>
2022-07-26 20:47:05 -0300

    Exercíco Quadrilateros

---
+++ src/exercicioII/Quadrilatero.java
@@ -0,0 +1,18 @@
+package exercicioII;
+
+public class Quadrilatero {
+    public static void area(double lado){
+        System.out.print(" Área do quadrado : " + lado * lado);
+    }
+    public  static void area(double lado1, double lado2) {
+        System.out.print(" Área do retangulo: " + lado1 * lado2);
+    }
+        public static void area(double baseMaior, double baseMenor, double altura) {
+            System.out.print(" Área do trapézio : " + ((baseMaior + baseMenor)*altura) /2);
+        }
+    public static void area(float diagonal1, float diagonal2){
+        System.out.print(" Area do losango: " + (diagonal1*diagonal2)/2);
+    }
+    }
+
+
---
+++ src/exercicioII/main.java
@@ -0,0 +1,12 @@
+package exercicioII;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Exercícios de Quadrilateros");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d, 5d);
+        Quadrilatero.area(7, 8, 9);
+        Quadrilatero.area(5f, 5f);
+    }
+
+}
